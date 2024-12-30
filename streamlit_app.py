207461772fa036d0861c410deb79814da14d2d23
Dylan Donohue <djodonohue@gmail.com>
2024-12-30 20:57:17 +0000

    changed to o1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,8 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+    "OpenAI - o1 model"
+    )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
@@ -41,7 +39,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="o1",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
